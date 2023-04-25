027db968442e2d806c82e1c31d66d486365e98a3
Taher_Elzoghby <tataamen678@gmail.com>
2023-04-25 23:49:46 +0200

    some_sums

--- src/problem/solving/ProblemSolving.java
+++ src/problem/solving/ProblemSolving.java
@@ -5,21 +5,7 @@ import java.util.Scanner;
 public class ProblemSolving {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        boolean flag=false;
-        for (int i = 0; i < num; i ++) {
-            for (int j = num - 1; j >= 0; j --) {
-                if (i%2==0&&j%2==0&&i+j==num) {
-                    flag =true;
-                    break;
-                }
-            }
-        }
-        if(flag)
-            System.out.println("YES");
-        else
-            System.out.println("No");
+        
     }
 
 }
