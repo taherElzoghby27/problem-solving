42c10a9d685af26b61d7e3d8105b290dc3434e88
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-17 12:26:15 +0300

    add simple mod problem

---
+++ .history/src/problem/solving/simpleMod_20240717122527.java
@@ -0,0 +1,12 @@
+package problem.solving;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class simpleMod {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        BigInteger n = input.nextBigInteger();
+        System.out.println("0 0");
+    }
+}
