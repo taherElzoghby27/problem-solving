11b6c10aa7e120d02f99385de3d1c853469174a0
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-18 14:03:06 +0300

    add summation problem

---
+++ .history/src/problem/solving/summation_20240718130119.java
@@ -0,0 +1,18 @@
+
+package problem.solving;
+
+import java.util.Scanner;
+
+public class summation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int sum = input.nextInt();
+        while ((--num) != 0) {
+            int n = input.nextInt();
+            sum += n;
+        }
+        System.out.println(sum);
+        System.out.println(Math.abs(sum));
+    }
+}
---
+++ src/problem/solving/summation.java
@@ -0,0 +1,18 @@
+
+package problem.solving;
+
+import java.util.Scanner;
+
+public class summation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long num = input.nextInt();
+        long sum = input.nextInt();
+        while ((--num) != 0) {
+            long n = input.nextInt();
+            sum += n;
+        }
+        System.out.println(Math.abs(sum));
+        System.out.println();
+    }
+}
