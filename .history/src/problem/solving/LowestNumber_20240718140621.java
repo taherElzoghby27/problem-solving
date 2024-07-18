f4b0d43eba818da5bc426af1bcb586050dd72f12
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-18 14:09:02 +0300

    add lowest number problem

---
+++ .history/src/problem/solving/LowestNumber_20240718140621.java
@@ -0,0 +1,23 @@
+package problem.solving;
+
+import java.util.Scanner;
+
+public class LowestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = input.nextInt();
+        }
+        int min = array[0];
+        int indexMin = 0;
+        for (int i = 1; i < num; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                indexMin = i;
+            }
+        }
+        System.out.println(min + " " + indexMin);
+    }
+}
---
+++ .history/src/problem/solving/LowestNumber_20240718140901.java
@@ -0,0 +1,24 @@
+package problem.solving;
+
+import java.util.Scanner;
+
+public class LowestNumber {
+    //E - Lowest Number : https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/E
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = input.nextInt();
+        }
+        int min = array[0];
+        int indexMin = 0;
+        for (int i = 1; i < num; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                indexMin = i;
+            }
+        }
+        System.out.println(min + " " + (indexMin + 1));
+    }
+}
