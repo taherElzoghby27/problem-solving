fff472cbb6bceab1882460633a3ccd6b57bc605f
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-29 14:20:24 +0300

    add ClimbingStairs problem

---
+++ .history/src/problem/solving/ClimbingStairs_20240729124122.java
@@ -0,0 +1,16 @@
+package problem.solving;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+    static int fib(int n) {
+        if (n <= 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(fib(6));
+    }
+}
---
+++ .history/src/problem/solving/ClimbingStairs_20240729124318.java
@@ -0,0 +1,16 @@
+package problem.solving;
+
+import java.util.Scanner;
+
+public class ClimbingStairs {
+    static int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(climbStairs(45));
+    }
+}
