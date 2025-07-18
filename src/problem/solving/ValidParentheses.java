6e995444828e7559bc688ae4d21f5bd1dd1049c6
taherElzoghby27 <tataamen678@gmail.com>
2025-07-18 10:09:56 +0300

    add valid stack problem

--- src/problem/solving/ValidParentheses.java
+++ src/problem/solving/ValidParentheses.java
@@ -3,12 +3,15 @@ package problem.solving;
 import java.util.Stack;
 
 public class ValidParentheses {
+
     public static boolean isValid(String s) {
-        if (s.isEmpty() || s.length() == 1)
+        if (s.isEmpty() || s.length() == 1) {
             return false;
+        }
         char first = s.charAt(0);
-        if (first == '}' || first == ')' || first == ']')
+        if (first == '}' || first == ')' || first == ']') {
             return false;
+        }
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
@@ -30,6 +33,21 @@ public class ValidParentheses {
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("(])"));
+        System.out.println(isValid2("]"));
+    }
+
+    public static boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!stack.isEmpty()
+                    && 
+                    (ch == ')' && stack.peek() == '(' || ch == '}' && stack.peek() == '{' || ch == ']' && stack.peek() == '[')) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.empty();
     }
 }
