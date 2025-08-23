61dfba633876dfa9c985053c329168894c6b497b
taherElzoghby27 <tataamen678@gmail.com>
2025-08-24 00:32:10 +0300

    IsPalindrome, LargestRectangleinHistogram, ThreeSum and TwoSumInputArrayIsSorted

---
+++ src/problem/solving/IsPalindrome.java
@@ -0,0 +1,52 @@
+package problem.solving;
+
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        String s = "race a car";
+        System.out.println(isPalindrome1(s));
+    }
+
+    public static boolean isPalindrome(String s) {//string builder
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int size = s.length();
+        if (s.isEmpty()) {
+            return true;
+        }
+        int updatedSize = 0;
+        if (size % 2 == 0) {
+            updatedSize = size / 2;
+        } else {
+            updatedSize = (size / 2) - 1;
+        }
+        if (updatedSize == 0) {
+            updatedSize = 1;
+        }
+        for (int i = 0; i < updatedSize; i++) {
+            if (s.codePointAt(i) != s.codePointAt(size - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome1(String s) {
+        int size = s.length();
+        int low = 0, high = size - 1;
+        while (low < high) {
+            // Skip non-alphanumeric characters
+            while (low < high && !Character.isLetterOrDigit(s.charAt(low))) {
+                ++low;
+            }
+            while (low < high && !Character.isLetterOrDigit(s.charAt(high))) {
+                --high;
+            }
+            if (Character.toLowerCase(s.charAt(low)) != Character.toLowerCase(s.charAt(high))) {
+                return false;
+            }
+            --high;
+            ++low;
+        }
+        return true;
+    }
+}
--- src/problem/solving/min_stack/CarFleet.java
+++ src/problem/solving/min_stack/CarFleet.java
@@ -1,6 +1,7 @@
 package problem.solving.min_stack;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class CarFleet {
 
@@ -12,16 +13,23 @@ public class CarFleet {
     }
 
     public static int carFleet(int target, int[] position, int[] speed) {
-        int size = position.length;
-        int[] times = new int[size];
-        for (int i = 0; i < size; i++) {
-            times[i] = (target - position[i]) / speed[i];
+        int n = position.length;
+        double[][] cars = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
         }
-        Arrays.sort(times);
-        for (int i = 0; i < size; i++) {
-            System.out.print(times[i] + " ");
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        Stack<Double> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            double time = (target - cars[i][0]) / cars[i][1];
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }
         }
-        return 1;
+
+        return stack.size();
     }
 
 }
